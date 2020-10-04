7de18f8edc331bbebf8fecad422944199e7938f0
Dom\Ivan <nt-polza@yandex.ru>
2020-10-04 12:13:35 +0500

    Фон, земля, управление персом

---
+++ project/Dangerous storage/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float horizontalInput;//Переменная получает -1 или 1
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime *speed * horizontalInput);//движение вправо влево
+    }
+}
